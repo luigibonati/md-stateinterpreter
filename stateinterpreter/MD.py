4ca45244a8afe8e1cf5f5f694207b9189aa51ba2
Pietro Novelli <pietronvll@gmail.com>
2021-09-28 15:57:13 +0200

    Colvar can now be pd datasets

--- stateinterpreter/MD.py
+++ stateinterpreter/MD.py
@@ -18,15 +18,22 @@ class Loader:
         self.kbt = 2.827
         traj_file = data_path + file_dict['trajectory']
         topo_file = data_path + file_dict['topology']
-        colvar_file = data_path + file_dict['collective_vars']
-        self.traj = md.load(traj_file, top=topo_file,stride=stride)
-        headers = pd.read_csv(colvar_file,sep=' ',skipinitialspace=True, nrows=0).columns[2:]
-        self.colvar = pd.read_csv(colvar_file,sep=' ',skipinitialspace=True, header=None,skiprows=1,names=headers,comment='#')  
+
+        if type(file_dict['collective_vars']) == pd.DataFrame:
+            self.colvar = file_dict['collective_vars']
+        elif type(file_dict['collective_vars']) == str:
+            colvar_file = data_path + file_dict['collective_vars']
+            pd_kwargs = file_dict.get('pd.read_csv.args', dict())
+            self.colvar = pd.read_csv(colvar_file,**pd_kwargs)  
+        else:
+            raise TypeError(f"Accepted types for collective_vars are \'pandas.Dataframe\' or \'str\'")
+
         self.colvar = self.colvar.iloc[::stride, :]
-        self.colvar.index = np.arange(len(self.colvar))
+        self.traj = md.load(traj_file, top=topo_file,stride=stride)
         self._DEV = _DEV
         assert len(self.traj) == len(self.colvar)
         
+    
     def approximate_FES(self, collective_vars, bounds, num=100):
         ndims = len(collective_vars)
         positions = np.array(self.colvar[collective_vars]).T
