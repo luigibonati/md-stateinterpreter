9b926b9cb290ad207700fb58f62488ec85ae66e7
Luigi Bonati <lui.bonati@gmail.com>
2021-10-01 02:32:20 +0200

    restructured md and added test for load functions

--- stateinterpreter/MD.py
+++ stateinterpreter/MD.py
@@ -1,42 +1,271 @@
-from tqdm import tqdm
-import pandas as pd
-import mdtraj as md
+# imports
+from typing import Type
 import numpy as np
-from .numerical_utils import gaussian_kde
+import pandas as pd
 import matplotlib.pyplot as plt
+import mdtraj as md
 import itertools
+
+from tqdm import tqdm
 from scipy.ndimage.filters import minimum_filter
 from scipy.ndimage.morphology import binary_erosion, generate_binary_structure
 from scipy.optimize import minimize
 
-class Loader:
-    def __init__(self, data_path, file_dict, stride=10, _DEV=False):
-        '''
-            file_dict = {'trajectory': '**.dcd', 'topology': '**.pdb', 'collective_vars': 'COLVAR' }
-        '''
-        self._base_path = data_path
-        self.kbt = 2.827
-        traj_file = data_path + file_dict['trajectory']
-        topo_file = data_path + file_dict['topology']
+from .numerical_utils import gaussian_kde
 
-        if type(file_dict['collective_vars']) == pd.DataFrame:
-            self.colvar = file_dict['collective_vars']
-        elif type(file_dict['collective_vars']) == str:
-            colvar_file = data_path + file_dict['collective_vars']
-            pd_kwargs = file_dict.get('pd.read_csv.args', dict())
-            self.colvar = pd.read_csv(colvar_file,**pd_kwargs)  
+"""
+OUTLINE
+=======
+1a. Load collective variables
+   - from FILE or pd.DataFrame
+1b. Load descriptors
+   - from FILE or pd.DataFrame
+2. (optional: load trajectory and compute descriptors)
+3. Compute FES --> assign states to configs
+4. Dataframe(CVs, descriptors, labels)
+"""
+
+"""
+TODO
+====
+- weights or logweights
+- basins or states?
+- name functions
+- mismatch approximate_fes (pietro)
+"""
+
+def is_plumed_file(filename):
+    """
+    Check if given file is in PLUMED format.
+
+    Parameters
+    ----------
+    filename : string, optional
+        PLUMED output file
+
+    Returns
+    -------
+    bool
+        wheter is a plumed output file
+    """   
+    headers = pd.read_csv( filename, sep=" ", skipinitialspace=True, nrows=0 )
+    is_plumed = True if " ".join(headers.columns[:2]) == "#! FIELDS" else False
+    return is_plumed
+
+def plumed_to_pandas(filename="./COLVAR"):
+    """
+    Load a PLUMED file and save it to a dataframe.
+
+    Parameters
+    ----------
+    filename : string, optional
+        PLUMED output file
+
+    Returns
+    -------
+    df : DataFrame
+        Collective variables dataframe
+    """
+    skip_rows = 1
+    # Read header 
+    headers = pd.read_csv( filename, sep=" ", skipinitialspace=True, nrows=0 )
+    # Discard #! FIELDS
+    headers = headers.columns[2:]
+    # Load dataframe and use headers for columns names
+    df = pd.read_csv(
+        filename,
+        sep=' ',
+        skipinitialspace=True,
+        header=None,
+        skiprows=range(skip_rows),
+        names=headers,
+        comment="#",
+    )
+
+    return df
+
+def load_dataframe(data, **kwargs):
+    """Load dataframe from object or from file.
+
+    Parameters
+    ----------
+    data : str or pandas.DataFrame
+        input data
+
+    Returns
+    -------
+    pandas.DataFrame
+        Dataframe
+
+    Raises
+    ------
+    TypeError
+        if data is not a valid type
+    """
+    # check if data is Dataframe
+    if type(data) == pd.DataFrame:
+        df = data
+    # or is a string
+    elif type(data) == str:
+        filename = data
+        # check if file is in PLUMED format
+        if is_plumed_file(filename):
+            df = plumed_to_pandas(filename)
+        # else use read_csv with optional kwargs
         else:
-            raise TypeError(f"Accepted types for collective_vars are \'pandas.Dataframe\' or \'str\'")
+            df = pd.read_csv(filename, **kwargs)
+    else:
+        raise TypeError(f"{data}: Accepted types are \'pandas.Dataframe\' or \'str\'")          
 
+    return df
+
+class Loader:
+
+    def __init__(self, colvar = None, descriptors = None, kbt = 2.5, stride=1, _DEV=False, **kwargs):
+        """Prepare inputs for stateinterpreter
+
+        Parameters
+        ----------
+        colvar : pandas.DataFrame or string
+            collective variables
+        descriptors : pandas.DataFrame or string, optional
+            input features, by default None
+        kbt : float, optional
+            temperature [KbT], by default 2.5
+        stride : int, optional
+            keep data every stride, by default 1
+        _DEV : bool, optional
+            enable dev. mode, by default False
+        """
+        # collective variables data
+        self.colvar = load_dataframe(colvar, **kwargs)      
         self.colvar = self.colvar.iloc[::stride, :]
-        self.traj = md.load(traj_file, top=topo_file,stride=stride)
-        self._DEV = _DEV
-        assert len(self.traj) == len(self.colvar), f"length traj ({len(self.traj)}) != length colvar ({len(self.colvar)})"
+        if _DEV:
+            print(f'Collective variables: {self.colvar.values.shape}')
+
+        # descriptors data
+        if descriptors is not None:
+            self.descriptors = load_dataframe(descriptors, **kwargs)
+            self.descriptors = self.descriptors.iloc[::stride, :]
+            if 'time' in self.descriptors.columns:
+                self.descriptors = self.descriptors.drop('time',axis='columns')
+            if _DEV:
+                print(f'Descriptors: {self.descriptors.shape}')
+            assert len(self.colvar) == len(self.descriptors), "mismatch between colvar and descriptor length."
         
+        # save attributes
+        self.kbt = kbt
+        self.stride = stride
+        self._DEV = _DEV
+
+        #initialize attributes to None
+        self.traj = None
+        self.basins = None
+
+    def load_trajectory(self, traj_dict):
+        """"Load trajectory with mdtraj.
+
+        Parameters
+        ----------
+        traj_dict : dict
+            dictionary containing trajectory and topology (optional) file
+        """
+        traj_file = traj_dict['trajectory']
+        topo_file = traj_dict['topology'] if 'topology' in traj_dict else None
+
+        self.traj = md.load(traj_file, top=topo_file, stride=self.stride)
+        
+        assert len(self.traj) == len(self.colvar), f"length traj ({len(self.traj)}) != length colvar ({len(self.colvar)})"
+
+    def compute_descriptors(self):
+        """ Compute descriptors from trajectory:
+        - Dihedral angles
+        - CA distances
+        - Hydrogen bonds
+
+        Raises
+        ------
+        KeyError
+            Trajectory needs to be set beforehand.
+        """
+        if self.traj == None:
+            raise KeyError('Trajectory not loaded. Call self.load_trajectory() first.')
+
+        ca = self._CA_DISTANCES()
+        hb = self._HYDROGEN_BONDS()
+        ang = self._ANGLES()
+
+        self.descriptors = pd.concat([ca,hb,ang], axis=1)
+        if self._DEV:
+            print(f'Descriptors: {self.descriptors.shape}')
+        assert len(self.colvar) == len(self.descriptors), "mismatch between colvar and descriptor length."
+
+    def identify_states(self, selected_cvs, bounds, weights=None, num=100, fes_cutoff=5, memory_saver=False, splits=50):
+        # retrieve weights
+        if weights is None:
+            if '.bias' in self.colvar.columns:
+                print('WARNING: a field with .bias is present in colvar, but it is not used for the FES.')
+        else:
+            if isinstance(weights,str):
+                w = self.colvar[weights].values
+            elif isinstance(weights,pd.DataFrame):
+                w = weights.values
+            elif isinstance(weights,np.ndarray):
+                w = weights
+            else:
+                raise TypeError(f"{weights}: Accepted types are \'pandas.Dataframe\', \'str\' or \'numpy.ndarray\' ")
+            if w.ndim != 1:
+                raise ValueError(f"{weights}: 1D array is required for weights")
+        
+        # store selected cvs
+        self.selected_cvs = selected_cvs
+        # compute fes
+        self.approximate_FES(selected_cvs, bounds, num=num, bw_method=None, weights=weights) 
+        # assign basins and select based on FES cutoff
+        self.basins = self._basin_selection(fes_cutoff=fes_cutoff, memory_saver=memory_saver,splits=splits)
+
+    def collect_data(self, only_selected_cvs = False):
+        """Prepare dataframe with: CVs, labels and descriptors
+
+        Parameters
+        ----------
+        only_selected_cvs : bool, optional
+            save only selected CVs for labeling
+
+        Returns
+        -------
+        pandas.DataFrame
+            dataset with all data
+
+        Raises
+        ------
+        KeyError
+            Basins labels needs to be set beforehand. 
+        """
+
+        if self.basins is None:
+            raise KeyError('Basins not selected. Call identify_states() first.')
+
+        return pd.concat( [
+            self.colvar[self.selected_cvs] if only_selected_cvs else self.colvar, 
+            self.basins, 
+            self.descriptors 
+            ], axis=1)
+
+    # DEPRECATED, TO REMOVE
+    def load(self, collective_vars, bounds, num=100, fes_cutoff=5, memory_saver=False, splits=50,bw_method=None,weights=None):
+        self.approximate_FES(collective_vars, bounds, num=num, bw_method=None, weights=None)
+        CVs = self.colvar[collective_vars]
+        basins = self._basin_selection(fes_cutoff=fes_cutoff, memory_saver=memory_saver,splits=splits)
+        CA_DIST = self._CA_DISTANCES()
+        HB = self._HYDROGEN_BONDS()
+        ANGLES = self._ANGLES()
+        return pd.concat([CVs, basins, CA_DIST,HB,ANGLES], axis=1)
+
     def approximate_FES(self, collective_vars, bounds, num=100, bw_method=None, weights=None):
         ndims = len(collective_vars)
         positions = np.array(self.colvar[collective_vars])
-        _FES = gaussian_kde(positions,bw_method=bw_method,weights=weights)
+        _FES = gaussian_kde(positions,bw_method=bw_method,weights=weights )
         self._FES_KDE = _FES
         _1d_samples = [np.linspace(vmin, vmax, num) for (vmin, vmax) in bounds]
         meshgrids = np.meshgrid(*_1d_samples)
@@ -278,7 +507,7 @@ class Loader:
         elif kind == 'chi2':
             dihedrals = md.compute_chi2(traj)
         else:
-            print( 'supported values: phi,psi,chi1,chi2' )
+            raise KeyError('supported values: phi,psi,chi1,chi2')
 
         idx_list = dihedrals[0]
         for i, idx in enumerate(idx_list):
@@ -310,16 +539,7 @@ class Loader:
     def contact_function(self, x,r0=1.,d0=0,n=6,m=12):
         # (see formula for RATIONAL) https://www.plumed.org/doc-v2.6/user-doc/html/switchingfunction.html
         return ( 1-np.power(((x-d0)/r0),n) ) / ( 1-np.power(((x-d0)/r0),m) )
-    
 
-    def load(self, collective_vars, bounds, num=100, fes_cutoff=5, memory_saver=False, splits=50,bw_method=None,weights=None):
-        self.approximate_FES(collective_vars, bounds, num=num, bw_method=None, weights=None)
-        CVs = self.colvar[collective_vars]
-        basins = self._basin_selection(fes_cutoff=fes_cutoff, memory_saver=memory_saver,splits=splits)
-        CA_DIST = self._CA_DISTANCES()
-        HB = self._HYDROGEN_BONDS()
-        ANGLES = self._ANGLES()
-        return pd.concat([CVs, basins, CA_DIST,HB,ANGLES], axis=1)
 
 
         
